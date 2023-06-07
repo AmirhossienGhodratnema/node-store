0575507a71668433ca368f760d01cb2a96b6ca63
amirhosseinghodratnema@gmail.com <amirhosseinghodratnema@gmail.com>
2023-06-07 11:46:50 +0330

    Create method getAll in productController

--- app/controller/api/admin/productController.js
+++ app/controller/api/admin/productController.js
@@ -18,20 +18,33 @@ module.exports = new class ProductController extends Controller {
                 req.body.image = path.join(req.body.fileUploadPath, req.body.filename);    //Add photo field in req.body.
                 req.body.image = req.body.image.replace(/\\/g, '/');    // Replace ( \\ ) to ( / ) for url.
             };
-
-            let { image, title, shortText, shortDescription, description, tags, category, price } = req.body
-            const product = await Product.create({ title, images: image, shortText, shortDescription, description, tags, category, price })
+            let { image, title, shortText, shortDescription, description, tags, category, price } = req.body;    // Get fiels in body.
+            await Product.create({ title, images: image, shortText, shortDescription, description, tags, category, price })    // Save the product in mongoDB.
             return res.status(200).json({
                 status: 200,
                 success: true,
                 message: 'The product has been successfully registered',
-                product
             });
         } catch (error) {
             if (req?.body?.fileUploadPath && req?.body?.filename) {
                 const { fileUploadPath, filename } = req.body;    // Get fiels for unlinkPhoto.
                 await unlinkPhoto(fileUploadPath, filename);     // Delete image.
             }
+            next(error);
+        };
+    };
+
+
+    async getAll(req, res, next) {
+        try {
+            const product = await Product.find({});
+            if (!product) throw { status: 400, message: 'There is no product' };
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                product
+            })
+        } catch (error) {
             next(error);
         };
     };
